ff6c6db897f4cc45933130380b7d135dbf54762b
sheila <sheilavp_a96@hotmail.com>
2024-01-13 17:02:56 -0600

    ftr: estilos a las card de poryectos

--- src/components/projects/ProjectInfo.jsx
+++ src/components/projects/ProjectInfo.jsx
@@ -1,4 +1,5 @@
-import { CardContent, CardMedia, Typography } from "@mui/material";
+import { CardMedia, Chip, Stack, Typography } from "@mui/material";
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
 import { projectList } from "./utils/list";
 import styles from "./styles.module.css";
 
@@ -23,12 +24,32 @@ export const ProjectInfo = () => {
             <div className={styles["card-text"]}>
               <div>
                 <Typography component="div" variant="h5" className={styles.name}>
-                  <strong>{project.name}</strong>
+                  <a href={project.link} target="_blank" rel="noreferrer">
+                    {project.name} <ArrowOutwardIcon />
+                  </a>
                 </Typography>
                 <Typography variant="subtitle1" color="text.secondary" component="div">
                   {project.description}
                 </Typography>
               </div>
+              {project.lenguaje.length > 0 && (
+                <Stack direction="row" spacing={1} sx={{ marginTop: "10px" }}>
+                  {project.lenguaje.map((leng) => (
+                    <Chip
+                      sx={{
+                        border: "1px solid #373a3c",
+                        color: "#373a3c",
+                        fontWeight: 700,
+                        fontSize: "14px",
+                      }}
+                      key={leng.name}
+                      label={leng.name}
+                      variant="outlined"
+                      className={styles.chip}
+                    />
+                  ))}
+                </Stack>
+              )}
             </div>
           </div>
         );
--- src/components/projects/utils/list.js
+++ src/components/projects/utils/list.js
@@ -6,11 +6,17 @@ export const projectList = [
     img: quiz,
     description:
       "Video course that teaches how to build a web app with the Spotify Web API. Topics covered include the principles of REST APIs, user auth flows, Node, Express, React, Styled Components, and more.",
+    lenguaje: [
+      {
+        name: "React Js",
+      },
+    ],
   },
   {
     name: "React quiz 2",
     link: "https://react-quiz.sheilavp.click/",
     img: quiz,
     description: "Video course that teaches how to build a web a.",
+    lenguaje: [],
   },
 ];
